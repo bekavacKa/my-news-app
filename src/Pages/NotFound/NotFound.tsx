73589e270ea878cf686bb8bd5316ddfa47e12be5
bekavacKa <th.beka17@gmail.com>
2023-03-15 22:53:12 +0100

    404 page

--- src/Pages/NotFound/NotFound.tsx
+++ src/Pages/NotFound/NotFound.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import routes from '../../Config/routes';
+import { FaArrowRight } from "react-icons/fa";
 import './not-found.scss';
 
 function NotFound() {
   return (
     <div className='not-found-w'>
-        NotFoundPage
+        <p className='not-found-info'>Page Not Found</p>
+        <Link className='not-found-btn' to={routes.HOME.url}>Go Home <FaArrowRight className='arrow' /> </Link>
     </div>
   )
 }
